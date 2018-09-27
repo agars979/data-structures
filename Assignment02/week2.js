1db8254ebc6a929f73992fc05c56aa6fe5060e1e
EC2 Default User <ec2-user@ip-172-31-45-172.us-east-2.compute.internal>
2018-09-27 16:51:21 +0000

    Update Assignment 02

--- Assignment02/week2.js
+++ Assignment02/week2.js
@@ -7,14 +7,18 @@ var content = fs.readFileSync('../Assignment01/zone09.txt');
 
 var $ = cheerio.load(content);
 
-var address = "";
+var address = [];
 
 $('tbody').last().find('tr').each(function(i, elem) {
     $(elem).find('td').first().contents().each(function(index, item) {
-        if (index >5 && index <9) {
-            address += ($(item).text());
+        if (index >5 && index <7) {
+            var parts = $(item).text().trim().split(",");
+            var entry = parts[0];
+            var location = new Object();
+            location.streetAddress = entry;
+            address.push(location);
         }
     });
 });
 
-fs.writeFileSync("./address.txt", address);
+fs.writeFileSync("./address.json", JSON.stringify(address));
